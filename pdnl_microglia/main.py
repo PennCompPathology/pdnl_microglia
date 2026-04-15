069cf56716d274fca0207c69800e2608314c3e8d
noahcapp <noahmcapp@gmail.com>
2026-04-15 14:22:16 -0400

    upgraded to use chunks rather than a big frame

--- pdnl_microglia/main.py
+++ pdnl_microglia/main.py
@@ -4,6 +4,7 @@ import sys
 
 import argparse
 import numpy as np
+from tqdm import tqdm
 import pdnl_sana.image
 import pdnl_sana.process
 import pdnl_sana.segment
@@ -12,109 +13,114 @@ from matplotlib import pyplot as plt
 
 def main():
 
-    debug = True
+    debug = False
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input_path', help="path to pdnl_process outputs", required=True)
     parser.add_argument('--checkpoint', action='store_true', help="saves intermediate data")
-    parser.add_argument('--model_path', help="path to ML model")
+    parser.add_argument('--model_path', help="path to ML model", default=None)
     args = parser.parse_args()
 
-    stain = pdnl_sana.image.Frame(os.path.join(args.input_path, 'stain.npy'))
-    frame = pdnl_sana.image.Frame(os.path.join(args.input_path, '..', 'frame.png'))
-    pos = pdnl_sana.image.Frame(os.path.join(args.input_path, 'pos.png'))
+    print('Processing chunks...', flush=True)
+    for d in tqdm(os.listdir(args.input_path)):
+        i, j = list(map(int, d.split('_')))
 
-    pos.img = pos.img // np.max(pos.img); pos.to_short()
+        in_d = os.path.join(args.input_path, f'{i}_{j}')
+        frame = pdnl_sana.image.Frame(os.path.join(in_d.replace('dab', 'chunks'), 'frame.png'))
+        stain = pdnl_sana.image.Frame(os.path.join(in_d, 'stain.npy'))
+        pos = pdnl_sana.image.Frame(os.path.join(in_d, 'pos.png'))
 
-    if args.checkpoint and os.path.exists(os.path.join(args.input_path, 'ctrs.npy')):
-        soma_ctrs = np.load(os.path.join(args.input_path, 'ctrs.npy'))
-    else:
-        soma_ctrs = pdnl_sana.segment.detect_somas(
-            pos, 
-            minimum_soma_radius=5,
-        )
-        if args.checkpoint:
-            np.save(os.path.join(args.input_path, 'ctrs.npy'), soma_ctrs)
+        pos.img = pos.img // np.max(pos.img); pos.to_short()
 
-    if args.checkpoint and os.path.exists(os.path.join(args.input_path, 'somas.png')):
-        soma_mask = pdnl_sana.image.Frame(os.path.join(args.input_path, 'somas.png'))
-    else:
-        soma_mask = pdnl_sana.segment.segment_somas(pos, soma_ctrs, n_directions=8, stride=4, sigma=3, fm_threshold=12, npools=1, debug=debug)
-        if args.checkpoint:
-            soma_mask.save(os.path.join(args.input_path, 'somas.png'))
-
-    soma_mask.img = (soma_mask.img / np.max(soma_mask.img)).astype(np.uint8)
-
-    soma_polys = soma_mask.to_polygons()[0]
-
-    # TODO: microglia_mask instead of pos, build this by thresholding pos.to_polygons() area
-
-    if args.checkpoint and os.path.exists(os.path.join(args.input_path, 'microglia')):
-        microglia_instances = []
-        for f in os.listdir(os.path.join(args.input_path, 'microglia')):
-            x = np.load(os.path.join(args.input_path, 'microglia', f), allow_pickle=True)
-
-            # TODO: handle level!
-            loc = pdnl_sana.geo.Point(*x['loc'], is_micron=False, level=0)
-            size = pdnl_sana.geo.Point(*x['size'], is_micron=False, level=0)
-            instance = pdnl_sana.segment.MicrogliaInstance(
-                soma=x['soma'], 
-                skeleton=x['skeleton'], 
-                mask=x['mask'], 
-                loc=loc, size=size
+        if args.checkpoint and os.path.exists(os.path.join(in_d, 'ctrs.npy')):
+            soma_ctrs = np.load(os.path.join(in_d, 'ctrs.npy'))
+        else:
+            soma_ctrs = pdnl_sana.segment.detect_somas(
+                pos, 
+                minimum_soma_radius=5,
+                debug=False,
             )
-            microglia_instances.append(instance)
+            if args.checkpoint:
+                np.save(os.path.join(in_d, 'ctrs.npy'), soma_ctrs)
 
-    else:
-        microglia_instances = pdnl_sana.segment.segment_microglia(pos, soma_polys, debug=debug)
-        if args.checkpoint:
-            d = os.path.join(os.path.join(args.input_path, 'microglia'))
-            if not os.path.exists(d):
-                os.makedirs(d)
-            for i, x in enumerate(microglia_instances):
-                x.save(os.path.join(d, f'{i}.npz'))
+        if args.checkpoint and os.path.exists(os.path.join(in_d, 'somas.png')):
+            soma_mask = pdnl_sana.image.Frame(os.path.join(in_d, 'somas.png'))
+        else:
+            soma_mask = pdnl_sana.segment.segment_somas(pos, soma_ctrs, n_directions=8, stride=4, sigma=3, fm_threshold=12, npools=1, debug=False)
+            if args.checkpoint:
+                soma_mask.save(os.path.join(in_d, 'somas.png'))
 
-    features = np.array([x.to_features() for x in microglia_instances])
+        soma_mask.img = (soma_mask.img / np.max(soma_mask.img)).astype(np.uint8)
 
-    import pickle
-    ss = pickle.load(open(os.path.join(args.model_path, 'ss_v2.pkl'), 'rb'))
-    clf = pickle.load(open(os.path.join(args.model_path, 'model_v2.pkl'), 'rb'))
+        soma_polys = soma_mask.to_polygons()[0]
 
-    print(features.shape)
-    features = ss.transform(features)
-    print(features.shape)
-    probs = clf.predict_proba(features)
-    probs = np.array([prob[:,1] for prob in probs]).T
-    print(probs.shape)
+        # TODO: microglia_mask instead of pos, build this by thresholding pos.to_polygons() area
+        if args.checkpoint and os.path.exists(os.path.join(in_d, 'microglia')):
+            microglia_instances = []
+            for f in os.listdir(os.path.join(in_d, 'microglia')):
+                x = np.load(os.path.join(in_d, 'microglia', f), allow_pickle=True)
 
-    if debug:
-        fig, axs = plt.subplots(2,2, sharex=True, sharey=True)
-        axs = axs.ravel()
+                # TODO: handle level!
+                loc = pdnl_sana.geo.Point(*x['loc'], is_micron=False, level=0)
+                size = pdnl_sana.geo.Point(*x['size'], is_micron=False, level=0)
+                instance = pdnl_sana.segment.MicrogliaInstance(
+                    soma=x['soma'], 
+                    skeleton=x['skeleton'], 
+                    mask=x['mask'], 
+                    loc=loc, size=size
+                )
+                microglia_instances.append(instance)
+        else:
+            microglia_instances = pdnl_sana.segment.segment_microglia(pos, soma_polys, debug=False)
+            if args.checkpoint:
+                d = os.path.join(os.path.join(in_d, 'microglia'))
+                if not os.path.exists(d):
+                    os.makedirs(d)
+                    for i, x in enumerate(microglia_instances):
+                        x.save(os.path.join(d, f'{i}.npz'))
 
-        # somas
-        axs[0].imshow(frame.img)
-        [axs[0].plot(*x.T, color='red') for x in soma_polys]
+        features = np.array([x.to_features() for x in microglia_instances])
 
-        # skeletons
-        skeleton_frame = frame.copy()
-        for x in microglia_instances:
-            x.overlay_skeleton(skeleton_frame, color=[255,0,0])
-        axs[1].imshow(skeleton_frame.img)
+        if not args.model_path is None:
+            import pickle
+            ss = pickle.load(open(os.path.join(args.model_path, 'ss_v2.pkl'), 'rb'))
+            clf = pickle.load(open(os.path.join(args.model_path, 'model_v2.pkl'), 'rb'))
+
+            features = ss.transform(features)
+            probs = clf.predict_proba(features)
+            probs = np.array([prob[:,1] for prob in probs]).T
+
+        if debug:
+            fig, axs = plt.subplots(2,2, sharex=True, sharey=True)
+            axs = axs.ravel()
+
+            # somas
+            axs[0].imshow(frame.img)
+            [axs[0].plot(*x.T, color='red') for x in soma_polys]
+
+            # skeletons
+            skeleton_frame = frame.copy()
+            for x in microglia_instances:
+                x.overlay_skeleton(skeleton_frame, color=[255,0,0])
+            axs[1].imshow(skeleton_frame.img)
         
-        # microglia
-        microglia_frame = frame.copy()
-        for x in microglia_instances:
-            x.overlay_microglia(microglia_frame, color=[255,0,0])
-        axs[2].imshow(microglia_frame.img)
+            # microglia
+            microglia_frame = frame.copy()
+            for x in microglia_instances:
+                x.overlay_microglia(microglia_frame, color=[255,0,0])
+            axs[2].imshow(microglia_frame.img)
 
-        # predictions
-        pred = frame.copy()
-        colors = [[255,0,0],[0,255,0],[0,0,255],[255,255,0],[0,255,255]]
-        for i, x in enumerate(microglia_instances):
-            x.overlay_prediction(pred, probs[i], colors)
-        axs[3].imshow(pred.img)
+            # predictions
+            if not args.model_path is None:
+                pred = frame.copy()
+                colors = [[255,0,0],[0,255,0],[0,0,255],[255,255,0],[0,255,255]]
+                for i, x in enumerate(microglia_instances):
+                    x.overlay_prediction(pred, probs[i], colors)
+                axs[3].imshow(pred.img)
 
-        plt.show()
+            plt.show()
+
+    # TODO: some kind of aggregation?
 
 
 if __name__ == "__main__":
